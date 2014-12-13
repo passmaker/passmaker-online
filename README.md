ae9c707ce9ce55018b0a86e79114ce0a42cfcbdd
kops <jeremie@dudie.fr>
2014-12-14 00:37:04 +0100

    Set up page templates, google api auth, a form for configuration edition

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-PasswordMaker NG
-================
+Secure Password Wallet
+======================
+
+Sepawall is inspired from [PasswordMaker](http://www.passwordmaker.org) concept: use one master password to generate unique password for each services.
+
+PasswordMaker drawbacks
+-----------------------
+
+PasswordMaker main drawback is that you have to remember your generator configuration: character set, password length, prefix, suffix, ...
+
+Sepawall alternative
+--------------------
+
+Sepawall aims to provide a way to store your password generator profile in the cloud (Google, Dropbox, OVH, Microsoft...).
--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,128 @@
-var pwmng = angular.module('passwordmaker-ng', [
+var sepawall = angular.module('sepawall', [
+  'ngRoute',
   'ui.bootstrap.dropdown',
-  'ui.bootstrap.tooltip'
+  'ui.bootstrap.tooltip',
+  'googleApi'
 ]);
 
-pwmng.controller('password-generator', function($scope) {
+sepawall.run(['$location', '$rootScope', function($location, $rootScope) {
+  $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+    if (current.hasOwnProperty('$$route')) {
+      $rootScope.title = current.$$route.title;
+    }
+  });
+}]);
+
+sepawall.config(function($routeProvider) {
+  $routeProvider
+    .when('/generator', {
+      title: 'Password generator',
+      templateUrl: 'partials/generator.html',
+      controller: 'PasswordGenerator'
+    })
+    .when('/configuration', {
+      title: 'Configuration editor',
+      templateUrl: 'partials/configuration.html',
+      controller: 'ConfigurationEditor'
+    }).otherwise({
+      redirectTo: '/generator'
+    });
+});
+
+sepawall.config(function(googleLoginProvider) {
+
+  googleLoginProvider.configure({
+    clientId: '452858183186-vnq9jcgt1heeveppo8nrnsvn1sep0q15.apps.googleusercontent.com',
+    scopes: [
+      'https://www.googleapis.com/auth/drive.file'
+    ]
+  });
+
+});
+
+sepawall.controller('storage-management', function($scope, googleLogin) {
+
+  $scope.login = function() {
+    googleLogin.login();
+  }
+
+});
+
+sepawall.controller('PasswordGenerator', function($scope) {
+
+  var makePassword = function(hashAlgorithm, key, data,  charset) {
+    return PasswordMaker_MD5.any_md5(key + data, charset)
+  };
+  
+  $scope.generatePassword = function() {
+    console.log('begin password generation');
+    var password = "";
+    var count = 0;
+    while (password.length < $scope.passwordLength) {
+      console.log('count: ' + count);
+      var key = $scope.masterPassword;
+      if (count > 0) {
+        key += '\n' + count;
+      }
+      password += makePassword($scope.hashAlgorithm,
+                         key,
+                         $scope.inputText + $scope.username + $scope.modifier,
+                         $scope.characters);
+      count++;
+    }
+    console.log('end password generation');
+    $scope.generatedPassword = password.substring(0, $scope.passwordLength);
+  };
+
+});
+
+sepawall.controller('ConfigurationEditor', function($scope) {
+
+  $scope.addException = function() {
+    $scope.conf.exceptions.push({
+      'service': 'New service name',
+      'passwordLength': '',
+      'modifier': ''
+    });
+  }
+
+  $scope.removeException = function(i) {
+    $scope.conf.exceptions.splice(i, 1);
+  }
+
+  $scope.saveConfiguration = function() {
+    console.log(JSON.stringify($scope.conf, null, ' '));
+  };
+
+  $scope.conf = {
+    'exceptions' : [ {
+      'service': 'facebook.com'
+    }, {
+      'service': 'likedin.com'
+    } ]
+  }
+
+});
+
+sepawall.controller('configuration-manager', function($scope) {
+
+  $scope.$on("googleDrive:loaded", function() {
+    console.log('google drive api loaded');
+  });
+
+  $scope.create = function() {
+    var request = gapi.client.request({
+     'path': '/drive/v2/files',
+     'method': 'POST',
+     'body': {
+       "title" : "Meta File 1.json",
+       "mimeType" : "application/json",
+       "description" : "This is a test of creating a metafile"
+      }
+    });
+    request.execute(function(file) {
+      console.log(file);
+    });
+  };
 
 });

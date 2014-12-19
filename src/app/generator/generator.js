b38283742f003ddbab52ab0b042d5421bff87ec1
kops <jeremie@dudie.fr>
2014-12-20 00:21:22 +0100

    Generator displays a hash as an helper to verify the master password

--- src/app/generator/generator.js
+++ src/app/generator/generator.js
@@ -12,11 +12,18 @@ angular.module('passmaker.generator', [
 })
 
 .controller('PasswordGeneratorCtrl', function($scope, profileManager, pMaker) {
-  
+
+  var pHashCheck = {
+    hashAlgorithm: 'sha1',
+    characters: '01',
+    passwordLength: 9
+  };
+
   $scope.generatePassword = function() {
     var p = profileManager.getProfile($scope.inputText);
     $scope.customProfile = p.custom;
     $scope.generatedPassword = pMaker.generate(p, $scope.masterPassword, $scope.inputText, $scope.username);
+    $scope.controlHash = parseInt(pMaker.generate(pHashCheck, $scope.masterPassword, 'hashcheck', ''), 2);
   };
 })
 

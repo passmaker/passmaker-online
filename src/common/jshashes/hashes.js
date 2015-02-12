f19917a401cb1df3a6de3996497ffdbb5c0d5d50
kops <jeremie@dudie.fr>
2015-02-12 23:52:31 +0100

    jhashes, fix jshint warnings

--- src/common/jshashes/hashes.js
+++ src/common/jshashes/hashes.js
@@ -170,7 +170,7 @@
 
   function rstr2binl(input) {
     var i, l = input.length * 8,
-      output = Array(input.length >> 2),
+      output = new Array (input.length >> 2),
       lo = output.length;
     for (i = 0; i < lo; i += 1) {
       output[i] = 0;
@@ -188,7 +188,7 @@
 
   function rstr2binb(input) {
     var i, l = input.length * 8,
-      output = Array(input.length >> 2),
+      output = new Array (input.length >> 2),
       lo = output.length;
     for (i = 0; i < lo; i += 1) {
       output[i] = 0;
@@ -205,11 +205,11 @@
 
   function rstr2any(input, encoding) {
     var divisor = encoding.length,
-      remainders = Array(),
+      remainders = [],
       i, q, x, ld, quotient, dividend, output, full_length;
 
     /* Convert to an array of 16-bit big-endian values, forming the dividend */
-    dividend = Array(Math.ceil(input.length / 2));
+    dividend = new Array (Math.ceil(input.length / 2));
     ld = dividend.length;
     for (i = 0; i < ld; i += 1) {
       dividend[i] = (input.charCodeAt(i * 2) << 8) | input.charCodeAt(i * 2 + 1);
@@ -222,7 +222,7 @@
      * All remainders are stored for later use.
      */
     while (dividend.length > 0) {
-      quotient = Array();
+      quotient = [];
       x = 0;
       for (i = 0; i < dividend.length; i += 1) {
         x = (x << 16) + dividend[i];
@@ -528,7 +528,7 @@
           bkey = binl(bkey, key.length * 8);
         }
 
-        ipad = Array(16), opad = Array(16);
+        ipad = new Array (16), opad = new Array (16);
         for (i = 0; i < 16; i += 1) {
           ipad[i] = bkey[i] ^ 0x36363636;
           opad[i] = bkey[i] ^ 0x5C5C5C5C;
@@ -631,7 +631,7 @@
           c = safe_add(c, oldc);
           d = safe_add(d, oldd);
         }
-        return Array(a, b, c, d);
+        return new Array (a, b, c, d);
       }
 
       /**
@@ -768,7 +768,7 @@
         if (bkey.length > 16) {
           bkey = binb(bkey, key.length * 8);
         }
-        ipad = Array(16), opad = Array(16);
+        ipad = new Array (16), opad = new Array (16);
         for (i = 0; i < 16; i += 1) {
           ipad[i] = bkey[i] ^ 0x36363636;
           opad[i] = bkey[i] ^ 0x5C5C5C5C;
@@ -783,7 +783,7 @@
 
       function binb(x, len) {
         var i, j, t, olda, oldb, oldc, oldd, olde,
-          w = Array(80),
+          w = new Array (80),
           a = 1732584193,
           b = -271733879,
           c = -1732584194,
@@ -822,7 +822,7 @@
           d = safe_add(d, oldd);
           e = safe_add(e, olde);
         }
-        return Array(a, b, c, d, e);
+        return new Array (a, b, c, d, e);
       }
 
       /**
@@ -961,8 +961,8 @@
         data = (utf8) ? utf8Encode(data) : data;
         var hash, i = 0,
           bkey = rstr2binb(key),
-          ipad = Array(16),
-          opad = Array(16);
+          ipad = new Array (16),
+          opad = new Array (16);
 
         if (bkey.length > 16) {
           bkey = binb(bkey, key.length * 8);
@@ -1205,8 +1205,8 @@
 
         var hash, i = 0,
           bkey = rstr2binb(key),
-          ipad = Array(32),
-          opad = Array(32);
+          ipad = new Array (32),
+          opad = new Array (32);
 
         if (bkey.length > 32) {
           bkey = binb(bkey, key.length * 8);
@@ -1231,82 +1231,82 @@
           hash = new Array(16),
           //Initial hash values
           H = [
-            new int64(0x6a09e667, -205731576),
-            new int64(-1150833019, -2067093701),
-            new int64(0x3c6ef372, -23791573),
-            new int64(-1521486534, 0x5f1d36f1),
-            new int64(0x510e527f, -1377402159),
-            new int64(-1694144372, 0x2b3e6c1f),
-            new int64(0x1f83d9ab, -79577749),
-            new int64(0x5be0cd19, 0x137e2179)
+            new Int64(0x6a09e667, -205731576),
+            new Int64(-1150833019, -2067093701),
+            new Int64(0x3c6ef372, -23791573),
+            new Int64(-1521486534, 0x5f1d36f1),
+            new Int64(0x510e527f, -1377402159),
+            new Int64(-1694144372, 0x2b3e6c1f),
+            new Int64(0x1f83d9ab, -79577749),
+            new Int64(0x5be0cd19, 0x137e2179)
           ],
-          T1 = new int64(0, 0),
-          T2 = new int64(0, 0),
-          a = new int64(0, 0),
-          b = new int64(0, 0),
-          c = new int64(0, 0),
-          d = new int64(0, 0),
-          e = new int64(0, 0),
-          f = new int64(0, 0),
-          g = new int64(0, 0),
-          h = new int64(0, 0),
+          T1 = new Int64(0, 0),
+          T2 = new Int64(0, 0),
+          a = new Int64(0, 0),
+          b = new Int64(0, 0),
+          c = new Int64(0, 0),
+          d = new Int64(0, 0),
+          e = new Int64(0, 0),
+          f = new Int64(0, 0),
+          g = new Int64(0, 0),
+          h = new Int64(0, 0),
           //Temporary variables not specified by the document
-          s0 = new int64(0, 0),
-          s1 = new int64(0, 0),
-          Ch = new int64(0, 0),
-          Maj = new int64(0, 0),
-          r1 = new int64(0, 0),
-          r2 = new int64(0, 0),
-          r3 = new int64(0, 0);
+          s0 = new Int64(0, 0),
+          s1 = new Int64(0, 0),
+          Ch = new Int64(0, 0),
+          Maj = new Int64(0, 0),
+          r1 = new Int64(0, 0),
+          r2 = new Int64(0, 0),
+          r3 = new Int64(0, 0);
 
         if (sha512_k === undefined) {
           //SHA512 constants
           sha512_k = [
-            new int64(0x428a2f98, -685199838), new int64(0x71374491, 0x23ef65cd),
-            new int64(-1245643825, -330482897), new int64(-373957723, -2121671748),
-            new int64(0x3956c25b, -213338824), new int64(0x59f111f1, -1241133031),
-            new int64(-1841331548, -1357295717), new int64(-1424204075, -630357736),
-            new int64(-670586216, -1560083902), new int64(0x12835b01, 0x45706fbe),
-            new int64(0x243185be, 0x4ee4b28c), new int64(0x550c7dc3, -704662302),
-            new int64(0x72be5d74, -226784913), new int64(-2132889090, 0x3b1696b1),
-            new int64(-1680079193, 0x25c71235), new int64(-1046744716, -815192428),
-            new int64(-459576895, -1628353838), new int64(-272742522, 0x384f25e3),
-            new int64(0xfc19dc6, -1953704523), new int64(0x240ca1cc, 0x77ac9c65),
-            new int64(0x2de92c6f, 0x592b0275), new int64(0x4a7484aa, 0x6ea6e483),
-            new int64(0x5cb0a9dc, -1119749164), new int64(0x76f988da, -2096016459),
-            new int64(-1740746414, -295247957), new int64(-1473132947, 0x2db43210),
-            new int64(-1341970488, -1728372417), new int64(-1084653625, -1091629340),
-            new int64(-958395405, 0x3da88fc2), new int64(-710438585, -1828018395),
-            new int64(0x6ca6351, -536640913), new int64(0x14292967, 0xa0e6e70),
-            new int64(0x27b70a85, 0x46d22ffc), new int64(0x2e1b2138, 0x5c26c926),
-            new int64(0x4d2c6dfc, 0x5ac42aed), new int64(0x53380d13, -1651133473),
-            new int64(0x650a7354, -1951439906), new int64(0x766a0abb, 0x3c77b2a8),
-            new int64(-2117940946, 0x47edaee6), new int64(-1838011259, 0x1482353b),
-            new int64(-1564481375, 0x4cf10364), new int64(-1474664885, -1136513023),
-            new int64(-1035236496, -789014639), new int64(-949202525, 0x654be30),
-            new int64(-778901479, -688958952), new int64(-694614492, 0x5565a910),
-            new int64(-200395387, 0x5771202a), new int64(0x106aa070, 0x32bbd1b8),
-            new int64(0x19a4c116, -1194143544), new int64(0x1e376c08, 0x5141ab53),
-            new int64(0x2748774c, -544281703), new int64(0x34b0bcb5, -509917016),
-            new int64(0x391c0cb3, -976659869), new int64(0x4ed8aa4a, -482243893),
-            new int64(0x5b9cca4f, 0x7763e373), new int64(0x682e6ff3, -692930397),
-            new int64(0x748f82ee, 0x5defb2fc), new int64(0x78a5636f, 0x43172f60),
-            new int64(-2067236844, -1578062990), new int64(-1933114872, 0x1a6439ec),
-            new int64(-1866530822, 0x23631e28), new int64(-1538233109, -561857047),
-            new int64(-1090935817, -1295615723), new int64(-965641998, -479046869),
-            new int64(-903397682, -366583396), new int64(-779700025, 0x21c0c207),
-            new int64(-354779690, -840897762), new int64(-176337025, -294727304),
-            new int64(0x6f067aa, 0x72176fba), new int64(0xa637dc5, -1563912026),
-            new int64(0x113f9804, -1090974290), new int64(0x1b710b35, 0x131c471b),
-            new int64(0x28db77f5, 0x23047d84), new int64(0x32caab7b, 0x40c72493),
-            new int64(0x3c9ebe0a, 0x15c9bebc), new int64(0x431d67c4, -1676669620),
-            new int64(0x4cc5d4be, -885112138), new int64(0x597f299c, -60457430),
-            new int64(0x5fcb6fab, 0x3ad6faec), new int64(0x6c44198c, 0x4a475817)
+            new Int64(0x428a2f98, -685199838), new Int64(0x71374491, 0x23ef65cd),
+            new Int64(-1245643825, -330482897), new Int64(-373957723, -2121671748),
+            new Int64(0x3956c25b, -213338824), new Int64(0x59f111f1, -1241133031),
+            new Int64(-1841331548, -1357295717), new Int64(-1424204075, -630357736),
+            new Int64(-670586216, -1560083902), new Int64(0x12835b01, 0x45706fbe),
+            new Int64(0x243185be, 0x4ee4b28c), new Int64(0x550c7dc3, -704662302),
+            new Int64(0x72be5d74, -226784913), new Int64(-2132889090, 0x3b1696b1),
+            new Int64(-1680079193, 0x25c71235), new Int64(-1046744716, -815192428),
+            new Int64(-459576895, -1628353838), new Int64(-272742522, 0x384f25e3),
+            new Int64(0xfc19dc6, -1953704523), new Int64(0x240ca1cc, 0x77ac9c65),
+            new Int64(0x2de92c6f, 0x592b0275), new Int64(0x4a7484aa, 0x6ea6e483),
+            new Int64(0x5cb0a9dc, -1119749164), new Int64(0x76f988da, -2096016459),
+            new Int64(-1740746414, -295247957), new Int64(-1473132947, 0x2db43210),
+            new Int64(-1341970488, -1728372417), new Int64(-1084653625, -1091629340),
+            new Int64(-958395405, 0x3da88fc2), new Int64(-710438585, -1828018395),
+            new Int64(0x6ca6351, -536640913), new Int64(0x14292967, 0xa0e6e70),
+            new Int64(0x27b70a85, 0x46d22ffc), new Int64(0x2e1b2138, 0x5c26c926),
+            new Int64(0x4d2c6dfc, 0x5ac42aed), new Int64(0x53380d13, -1651133473),
+            new Int64(0x650a7354, -1951439906), new Int64(0x766a0abb, 0x3c77b2a8),
+            new Int64(-2117940946, 0x47edaee6), new Int64(-1838011259, 0x1482353b),
+            new Int64(-1564481375, 0x4cf10364), new Int64(-1474664885, -1136513023),
+            new Int64(-1035236496, -789014639), new Int64(-949202525, 0x654be30),
+            new Int64(-778901479, -688958952), new Int64(-694614492, 0x5565a910),
+            new Int64(-200395387, 0x5771202a), new Int64(0x106aa070, 0x32bbd1b8),
+            new Int64(0x19a4c116, -1194143544), new Int64(0x1e376c08, 0x5141ab53),
+            new Int64(0x2748774c, -544281703), new Int64(0x34b0bcb5, -509917016),
+            new Int64(0x391c0cb3, -976659869), new Int64(0x4ed8aa4a, -482243893),
+            new Int64(0x5b9cca4f, 0x7763e373), new Int64(0x682e6ff3, -692930397),
+            new Int64(0x748f82ee, 0x5defb2fc), new Int64(0x78a5636f, 0x43172f60),
+            new Int64(-2067236844, -1578062990), new Int64(-1933114872, 0x1a6439ec),
+            new Int64(-1866530822, 0x23631e28), new Int64(-1538233109, -561857047),
+            new Int64(-1090935817, -1295615723), new Int64(-965641998, -479046869),
+            new Int64(-903397682, -366583396), new Int64(-779700025, 0x21c0c207),
+            new Int64(-354779690, -840897762), new Int64(-176337025, -294727304),
+            new Int64(0x6f067aa, 0x72176fba), new Int64(0xa637dc5, -1563912026),
+            new Int64(0x113f9804, -1090974290), new Int64(0x1b710b35, 0x131c471b),
+            new Int64(0x28db77f5, 0x23047d84), new Int64(0x32caab7b, 0x40c72493),
+            new Int64(0x3c9ebe0a, 0x15c9bebc), new Int64(0x431d67c4, -1676669620),
+            new Int64(0x4cc5d4be, -885112138), new Int64(0x597f299c, -60457430),
+            new Int64(0x5fcb6fab, 0x3ad6faec), new Int64(0x6c44198c, 0x4a475817)
           ];
         }
 
         for (i = 0; i < 80; i += 1) {
-          W[i] = new int64(0, 0);
+          W[i] = new Int64(0, 0);
         }
 
         // append padding to the source string. The format is described in the FIPS.
@@ -1400,7 +1400,7 @@
 
       //A constructor for 64-bit numbers
 
-      function int64(h, l) {
+      function Int64(h, l) {
         this.h = h;
         this.l = l;
         //this.toString = int64toString;
@@ -1612,8 +1612,8 @@
         data = (utf8) ? utf8Encode(data) : data;
         var i, hash,
           bkey = rstr2binl(key),
-          ipad = Array(16),
-          opad = Array(16);
+          ipad = new Array (16),
+          opad = new Array (16);
 
         if (bkey.length > 16) {
           bkey = binl(bkey, key.length * 8);
